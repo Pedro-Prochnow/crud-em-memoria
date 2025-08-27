f566c8e682ffc4033fd5d4501b78516640396126
Pedro Prochnow <pedrohenriqueprochnow@gmail.com>
2025-08-27 20:49:56 -0300

    terminei a rota delete

--- index.js
+++ index.js
@@ -22,25 +22,25 @@ app.get("/usuarios", (req, res) => {
 app.post("/usuarios", (req, res) => {
     const dados = req.body;
 
-    // Se for um array de usuários
-    if (Array.isArray(dados)) {
-        const novosUsuarios = [];
+    // // Se for um array de usuários
+    // if (Array.isArray(dados)) {
+    //     const novosUsuarios = [];
 
-        for (const usuario of dados) {
-            const { nome, email } = usuario;
+    //     for (const usuario of dados) {
+    //         const { nome, email, } = usuario;
 
-            if (!nome || !email) {
-                return res.status(400).json({ Mensagem: "Todos os usuários devem ter nome e email" });
-            }
+    //         if (!nome || !email) {
+    //             return res.status(400).json({ Mensagem: "Todos os usuários devem ter nome e email" });
+    //         }
 
-            ultimoId += 1;
-            const novoUsuario = { id: ultimoId, nome, email };
-            usuarios.push(novoUsuario);
-            novosUsuarios.push(novoUsuario);
-        }
+    //         ultimoId += 1;
+    //         const novoUsuario = { id: ultimoId, nome, email };
+    //         usuarios.push(novoUsuario);
+    //         novosUsuarios.push(novoUsuario);
+    //     }
 
-        return res.status(201).json(novosUsuarios);
-    }
+    //     return res.status(201).json(novosUsuarios.id);
+    // }
 
     // Se for um único usuário
     const { nome, email } = dados;
@@ -53,10 +53,36 @@ app.post("/usuarios", (req, res) => {
     const novoUsuario = { id: ultimoId, nome, email };
     usuarios.push(novoUsuario);
 
-    res.status(201).json(novoUsuario);
+    res.status(201).json(novoUsuario.id);
 });
 
 // criar uma rota para deletar um usuario
+app.delete('/usuarios/:id', (req, res) => {
+    const id = req.params.id;
+    const idNumerico = parseInt(id);
+
+    //se n for um numero vai retornar essa msg
+    if(isNaN(idNumerico)) {
+        return  res.status(400).json({mensagem: "ID invalido"})
+    }
+
+    //esse treecho tá perguntando se achou alguém com o id requerido na url no postman
+    let posicao_do_usuario = usuarios.findIndex(
+        (usuario) => usuario.id === idNumerico
+    );
+
+
+    if (posicao_do_usuario === -1){
+        return  res.status(404).json({mensagem:"Usuario não existe"});
+    }
+
+    //vai pegar o array 'posicao_do_usuario e deleta-lo
+    //o 1 tá falando quanto é pra apagar se fosse 2 apagaria o escolhido e o proximo
+    usuarios.splice(posicao_do_usuario, 1); 
+    res.status(204).send();
+
+});
 // criar uma rota para atualizar um usuario
+//
 
 app.listen(3000);
